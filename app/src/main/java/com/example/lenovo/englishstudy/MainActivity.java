8c44e814f5ebd225f183416fb938d866e628981f
wangaiji <1670142221@qq.com>
2019-01-23 18:37:24 +0800

    2019.1.23
    换成了bottomnavigationbar

--- app/src/main/java/com/example/lenovo/englishstudy/MainActivity.java
+++ app/src/main/java/com/example/lenovo/englishstudy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lenovo.englishstudy;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationMenuView;
@@ -9,9 +10,12 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.example.lenovo.englishstudy.fragment.ChatroomFragment;
 import com.example.lenovo.englishstudy.fragment.HomeFragment;
 import com.example.lenovo.englishstudy.fragment.SearchFragment;
@@ -19,82 +23,120 @@ import com.example.lenovo.englishstudy.fragment.UserFragment;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
-    private ViewPager viewPager;
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener{
+    private BottomNavigationBar bottomNavigationBar;
+    private int lastSelectedPosition = 0;
+    private String TAG = MainActivity.class.getSimpleName();
+    private HomeFragment homeFragment;
+    private SearchFragment searchFragment;
+    private ChatroomFragment chatroomFragment;
+    private UserFragment userFragment;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.navigation);
-        viewPager = findViewById(R.id.viewPager);
+        bottomNavigationBar = findViewById(R.id.navigation_bar);
 
-        //设置点击监听
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    //根据navagatin.xml中item的id进行case
-                    case R.id.navigation_home:
-                        viewPager.setCurrentItem(0);
-                        //跳到对应ViewPager的page
-                        break;
-                    case R.id.navigation_search:
-                        viewPager.setCurrentItem(1);
+        /** 导航基础设置 包括按钮选中效果 导航栏背景色等 */
+        bottomNavigationBar
+                .setTabSelectedListener(this)
+                .setMode(BottomNavigationBar.MODE_FIXED)
+                /**
+                 *  setMode() 内的参数有三种模式类型：
+                 *  MODE_DEFAULT 自动模式：导航栏Item的个数<=3 用 MODE_FIXED 模式，否则用 MODE_SHIFTING 模式
+                 *  MODE_FIXED 固定模式：未选中的Item显示文字，无切换动画效果。
+                 *  MODE_SHIFTING 切换模式：未选中的Item不显示文字，选中的显示文字，有切换动画效果。
+                 */
 
-                        break;
-                    case R.id.navigation_chatroom:
-                        viewPager.setCurrentItem(2);
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC)
+                /**
+                 *  setBackgroundStyle() 内的参数有三种样式
+                 *  BACKGROUND_STYLE_DEFAULT: 默认样式 如果设置的Mode为MODE_FIXED，将使用BACKGROUND_STYLE_STATIC
+                 *                                    如果Mode为MODE_SHIFTING将使用BACKGROUND_STYLE_RIPPLE。
+                 *  BACKGROUND_STYLE_STATIC: 静态样式 点击无波纹效果
+                 *  BACKGROUND_STYLE_RIPPLE: 波纹样式 点击有波纹效果
+                 */
 
-                        break;
-                    case R.id.navigation_user:
-                        viewPager.setCurrentItem(3);
-                }
-                return false;
-            }
-        });
+                .setActiveColor("#11CDB5")//选中颜色
+                .setInActiveColor("#7b7a7a") //未选中颜色
+                .setBarBackgroundColor("#ffffff");//导航栏背景色
 
-        //底部导航栏有几项就有几个Fragment
-        final ArrayList<Fragment> fragments = new ArrayList<>(4);
-        fragments.add(new HomeFragment());
-        fragments.add(new SearchFragment());
-        fragments.add(new ChatroomFragment());
-        fragments.add(new UserFragment());
+        /** 添加导航按钮 */
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_home, "Home"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_search, "Search"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_chatroom, "Chatroom"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_user, "User"))
+                .setFirstSelectedPosition(lastSelectedPosition )
+                .initialise(); //initialise 一定要放在 所有设置的最后一项
 
-        FragmentStatePagerAdapter mPagerAdapter=new FragmentStatePagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                return fragments.get(position);  //得到Fragment
-            }
+        init();
 
-            @Override
-            public int getCount() {
-                return fragments.size();  //得到数量
-            }
-        };
-        viewPager.setAdapter(mPagerAdapter);   //设置适配器
-        viewPager.setOffscreenPageLimit(3); //预加载剩下三页
+    }
 
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
+    private void init(){
+        homeFragment = new HomeFragment();
+        searchFragment = new SearchFragment();
+        chatroomFragment = new ChatroomFragment();
+        userFragment = new UserFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout,homeFragment).add(R.id.frameLayout,searchFragment).add(R.id.frameLayout,chatroomFragment).add(R.id.frameLayout,userFragment);
+        fragmentTransaction.hide(homeFragment).hide(searchFragment).hide(chatroomFragment).hide(userFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
 
-            }
 
-            @Override
-            public void onPageSelected(int position) {
-                bottomNavigationView.getMenu().getItem(position).setChecked(true);
-            }
+    /**
+     * 设置导航选中的事件
+     */
+    @Override
+    public void onTabSelected(int position) {
+        FragmentTransaction beginTransaction = getSupportFragmentManager().beginTransaction();
+        switch (position){
+            case 0:
+                beginTransaction.hide(searchFragment).hide(chatroomFragment).hide(userFragment);
+                beginTransaction.show(homeFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case 1:
+                beginTransaction.hide(homeFragment).hide(chatroomFragment).hide(userFragment);
+                beginTransaction.show(searchFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case 2:
+                beginTransaction.hide(homeFragment).hide(searchFragment).hide(userFragment);
+                beginTransaction.show(chatroomFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case 3:
+                beginTransaction.hide(homeFragment).hide(searchFragment).hide(chatroomFragment);
+                beginTransaction.show(userFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+        }
+    }
 
-            @Override
-            public void onPageScrollStateChanged(int i) {
+    /**
+     * 设置未选中Fragment 事务
+     */
+    @Override
+    public void onTabUnselected(int position) {
 
-            }
-        });
+    }
+
+    /**
+     * 设置释放Fragment 事务
+     */
+    @Override
+    public void onTabReselected(int position) {
+
     }
 
 }
