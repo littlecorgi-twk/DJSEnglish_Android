01fbb9a079f099d0d04a200b33fffb357dc58c4d
wangaiji <1670142221@qq.com>
2019-02-28 12:06:42 +0800

    用自定义view实现我的界面
    2019.2.28

---
+++ app/src/main/java/com/example/lenovo/englishstudy/MyView.java
@@ -0,0 +1,205 @@
+package com.example.lenovo.englishstudy;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MyView extends LinearLayout {
+    //各各控件
+    private View dividerBottom;
+    private LinearLayout llRoot;
+    private ImageView ivLeftIcon;
+    private TextView tvTextContent;
+    private ImageView ivRightIcon;
+
+    public MyView(Context context) {  //该构造函数使其，可以在Java代码中创建
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {//该构造函数使其可以在XML布局中使用
+        super(context, attrs);
+    }
+
+    /**
+     * 初始化各个控件
+     */
+    public MyView init() {
+        //引入之前的xml布局
+        LayoutInflater.from(getContext()).inflate(R.layout.item_view, this, true);
+        llRoot = findViewById(R.id.ll_root);
+        dividerBottom = findViewById(R.id.divider_bottom);
+        ivLeftIcon = findViewById(R.id.iv_left_icon);
+        tvTextContent = findViewById(R.id.tv_text_content);
+        ivRightIcon = findViewById(R.id.iv_right_icon);
+        return this;
+    }
+
+    public MyView init(int iconRes, String textContent) {
+        init();
+        setLeftIcon(iconRes);
+        setTextContent(textContent);
+        showArrow(true);
+        return this;
+
+    }
+
+    public MyView initMine(int iconRes, String textContent, boolean showDivider) {
+        init(iconRes, textContent);
+        showDivider(showDivider);
+        return this;
+    }
+
+    public MyView showDivider(Boolean showDivderBottom) {
+        if (showDivderBottom) {
+            dividerBottom.setVisibility(VISIBLE);
+        } else {
+            dividerBottom.setVisibility(GONE);
+        }
+        return this;
+    }
+
+    //右箭头
+    public MyView showArrow(boolean showArrow) {
+        if (showArrow) {
+            ivRightIcon.setVisibility(VISIBLE);
+        } else {
+            ivRightIcon.setVisibility(GONE);
+        }
+        return this;
+
+    }
+
+    /**
+     * 设置root的paddingTop 与 PaddingBottom 从而控制整体的行高
+     * paddingLeft 与 paddingRight 保持默认 20dp
+     */
+    public MyView setRootPaddingTopBottom(int paddintTop, int paddintBottom) {
+        llRoot.setPadding(DensityUtils.dp2px(getContext(), 20),
+                DensityUtils.dp2px(getContext(), paddintTop),
+                DensityUtils.dp2px(getContext(), 20),
+                DensityUtils.dp2px(getContext(), paddintBottom));
+        return this;
+    }
+
+    /**
+     * 设置root的paddingLeft 与 PaddingRight 从而控制整体的行高
+     * <p>
+     * paddingTop 与 paddingBottom 保持默认 15dp
+     */
+    public MyView setRootPaddingLeftRight(int paddintTop, int paddintRight) {
+        llRoot.setPadding(DensityUtils.dp2px(getContext(), paddintTop),
+                DensityUtils.dp2px(getContext(), 15),
+                DensityUtils.dp2px(getContext(), paddintRight),
+                DensityUtils.dp2px(getContext(), 15));
+        return this;
+    }
+
+    /**
+     * 设置左边Icon
+     *
+     * @param iconRes
+     */
+    public MyView setLeftIcon(int iconRes) {
+        ivLeftIcon.setImageResource(iconRes);
+        return this;
+    }
+
+    /**
+     * 设置左边Icon显示与否
+     *
+     * @param showLeftIcon
+     */
+    public MyView showLeftIcon(boolean showLeftIcon) {
+        if (showLeftIcon) {
+            ivLeftIcon.setVisibility(VISIBLE);
+        } else {
+            ivLeftIcon.setVisibility(GONE);
+        }
+        return this;
+    }
+
+    /**
+     * 设置右边Icon 以及Icon的宽高
+     */
+    public MyView setLeftIconSize(int widthDp, int heightDp) {
+        ViewGroup.LayoutParams layoutParams = ivLeftIcon.getLayoutParams();
+        layoutParams.width = DensityUtils.dp2px(getContext(), widthDp);
+        layoutParams.height = DensityUtils.dp2px(getContext(), heightDp);
+        ivLeftIcon.setLayoutParams(layoutParams);
+        return this;
+    }
+
+
+    /**
+     * 设置中间的文字内容
+     *
+     * @param textContent
+     * @return
+     */
+    public MyView setTextContent(String textContent) {
+        tvTextContent.setText(textContent);
+        return this;
+    }
+
+    /**
+     * 设置中间的文字颜色
+     *
+     * @return
+     */
+    public MyView setTextContentColor(int colorRes) {
+        tvTextContent.setTextColor(getResources().getColor(colorRes));
+        return this;
+    }
+
+    /**
+     * 设置中间的文字大小
+     *
+     * @return
+     */
+    public MyView setTextContentSize(int textSizeSp) {
+        tvTextContent.setTextSize(textSizeSp);
+        return this;
+    }
+
+
+
+    /**
+     * 整个一行被点击
+     */
+    public static interface OnRootClickListener {
+        void onRootClick(View view);
+    }
+    /**
+     * 右边箭头的点击事件
+     */
+    public static interface OnArrowClickListener {
+        void onArrowClick(View view);
+    }
+    public MyView setOnRootClickListener(final OnRootClickListener onRootClickListener, final int tag) {
+        llRoot.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llRoot.setTag(tag);
+                onRootClickListener.onRootClick(llRoot);
+            }
+        });
+        return this;
+    }
+    public MyView setOnArrowClickListener(final OnArrowClickListener onArrowClickListener, final int tag) {
+
+        ivRightIcon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ivRightIcon.setTag(tag);
+                onArrowClickListener.onArrowClick(ivRightIcon);
+            }
+        });
+        return this;
+    }
+
+}
--- app/src/main/java/com/example/lenovo/englishstudy/fragment/UserFragment.java
+++ app/src/main/java/com/example/lenovo/englishstudy/fragment/UserFragment.java
@@ -1,6 +1,75 @@
 package com.example.lenovo.englishstudy.fragment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class UserFragment extends Fragment {
+import com.example.lenovo.englishstudy.MyView;
+import com.example.lenovo.englishstudy.R;
+
+public class UserFragment extends Fragment implements MyView.OnRootClickListener {
+    private LinearLayout oneItem;
+    private LinearLayout twoItem;
+    private LinearLayout threeItem;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.userfragment, container, false);
+        oneItem = view.findViewById(R.id.one_item);
+        twoItem = view.findViewById(R.id.two_item);
+        threeItem = view.findViewById(R.id.three_item);
+        initView();
+        return view;
+    }
+
+    public void initView() {
+        //添加我的消息
+        oneItem.addView(new MyView(getContext())
+                .initMine(R.drawable.ic_message, "我的消息", true)
+                .setOnRootClickListener(this, 1));
+        //添加我的收藏
+        oneItem.addView(new MyView(getContext())
+                .initMine(R.drawable.ic_collect, "我的收藏夹", true)
+                .setOnRootClickListener(this, 2));
+        //添加我的评论
+        oneItem.addView(new MyView(getContext())
+                .initMine(R.drawable.ic_comment, "我的评论", false)
+                .setOnRootClickListener(this, 3));
+
+        //添加帮助反馈
+        twoItem.addView(new MyView(getContext())
+                .initMine(R.drawable.ic_help, "帮助反馈", true)
+                .setOnRootClickListener(this, 4));
+
+        //添加推荐美文
+        twoItem.addView(new MyView(getContext())
+                .initMine(R.drawable.ic_recommend, "推荐美文", false)
+                .setOnRootClickListener(this, 5));
+
+        //添加设置
+        threeItem.addView(new MyView(getContext())
+                .initMine(R.drawable.ic_set, "设置", false)
+                .setOnRootClickListener(this, 6));
+
+    }
+
+    @Override
+    public void onRootClick(View view) {
+        switch ((int) view.getTag()) {
+            case 1:
+                //   startActivity(new Intent(getContext(), MyDownloadActivity.class));
+                break;
+            case 2:
+                //    startActivity(new Intent(getContext(), MyCollectionActivity.class));
+                break;
+        }
+
+
+    }
 }
