9007b9b0b0fa1677f4bbfde9f7bfdf63f95ea0dc
wangaiji <1670142221@qq.com>
2019-03-16 20:10:32 +0800

    检测是否第一次登录及修改点击事件，增加了popupwindow
    2019.3.16

--- app/src/main/java/com/example/lenovo/englishstudy/SearchHistoryActivity.java
+++ app/src/main/java/com/example/lenovo/englishstudy/SearchHistoryActivity.java
@@ -1,16 +1,21 @@
 package com.example.lenovo.englishstudy;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import com.example.lenovo.englishstudy.db.Sentence;
 import com.example.lenovo.englishstudy.userdefined.FlowLayout;
@@ -28,6 +33,7 @@ public class SearchHistoryActivity extends AppCompatActivity {
     private List<Sentence> sentenceList;
     private Set<String> dataList = new HashSet<>();
     private FlowLayout mFlowLayout;
+    private TextView sentence, translate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,8 @@ public class SearchHistoryActivity extends AppCompatActivity {
                 delete.setVisibility(View.INVISIBLE);
             }
         });
+
+
 
     }
 
@@ -94,8 +102,31 @@ public class SearchHistoryActivity extends AppCompatActivity {
             textView.setTextColor(Color.BLACK);
             textView.setBackgroundDrawable(getResources().getDrawable(R.drawable.textview));
             mFlowLayout.addView(textView, lp);
-
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showCenterPopupWindow(textView);
+                }
+            });
         }
+    }
+
+    public void showCenterPopupWindow (TextView textView) {
+        View contentView = LayoutInflater.from(SearchHistoryActivity.this).inflate(R.layout.popupwindow, null);
+        PopupWindow popupWindow = new PopupWindow(contentView, 950, LinearLayout.LayoutParams.WRAP_CONTENT,false);
+        sentence = contentView.findViewById(R.id.sentence);
+        translate = contentView.findViewById(R.id.translate);
+        sentence.setText(textView.getText());
+        popupWindow.setOutsideTouchable(true);
+        // 设置PopupWindow是否能响应外部点击事件
+        popupWindow.setTouchable(true);
+        // 设置PopupWindow是否能响应点击事件
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        // 设置PopupWindow背景
+        popupWindow.setAnimationStyle(R.style.anim_popup_center);
+        //设置动画
+        popupWindow.showAtLocation(contentView, Gravity.CENTER, 0, 0);
+
     }
 
 }
