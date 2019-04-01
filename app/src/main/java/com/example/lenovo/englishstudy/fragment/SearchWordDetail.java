0a38cf46913be982b2b632e4e86d9949cd95b1f5
a1203991686 <a1203991686@126.com>
2019-04-01 20:47:12 +0800

    添加单词搜索详情页

---
+++ app/src/main/java/com/example/lenovo/englishstudy/fragment/SearchWordDetail.java
@@ -0,0 +1,15 @@
+package com.example.lenovo.englishstudy.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lenovo.englishstudy.R;
+
+public class SearchWordDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_word_detail);
+    }
+}
